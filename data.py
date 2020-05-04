2902682dda33bc0bfc1af6d5a691be6991be0954
Ganesh N <ganeshn.4488@gmail.com>
2020-05-04 13:16:39 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,134 @@
+import yfinance as yf
+from datetime import datetime
+# Get the data for the stock AAPL
+from PriceIndices import Indices
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+
+#Imports
+import time
+#Assign time.time() object to "start" so we can profile the code.
+start = time.time()
+import pandas as pd
+import numpy as np
+from newspaper import Article
+
+import json
+
+try:
+    import urllib.parse as urlparse
+except ImportError:
+    import urlparse
+import requests
+
+
+
+
+def get_coin_data(crypto='AMZN', start_date='2013-04-28', end_date='2020-04-28', save_data=None):
+    df2 = yf.download(crypto, start_date, end_date)
+    df2['Date'] = df2.index
+
+    df = df2[['Date', 'Close']]
+    df.columns = ['date', 'price']
+    #print(df)
+    df_bi = Indices.get_simple_moving_average(df)
+    df_bi.drop('price', axis=1, inplace=True)
+
+
+    df = pd.merge(df, df_bi, on='date', how='left')
+    #print(df)
+    del df_bi
+
+    for col in df.columns[1:]:
+        df[col] = np.round(df[col], 2)
+    while save_data:
+        df.to_csv('data.csv', index=False)
+        break
+    return df
+
+
+
+def get_news_headlines(url):
+    latestheadlines_links = []
+    parsed_uri = urlparse.urljoin(url, '/')
+
+    try:
+
+        req = requests.get(url)
+        html = req.text
+        soup = BeautifulSoup(html, 'html.parser')
+        links = soup.findAll('a')
+
+
+        for i in links:
+            if '/news/' in i['href'] or '/m/' in i['href']:
+                l = parsed_uri.rstrip('/') + i['href']
+                latestheadlines_links.append(l)
+
+    except requests.exceptions.RequestException as re:
+        print("Exception: can't crawl web site (%s)" % re)
+        pass
+
+    return latestheadlines_links
+
+
+def get_articles(links):
+    # Intialize list articles_info list
+    articles_info = []
+    for i in links:
+        # Intialize dictionary
+        article_dict = {}
+        # Insert link "i" into the dictionary
+        article_dict["link"] = i
+        # Pass link into Article() function
+        art = Article(i)
+        # Download contents of art object
+        art.download()
+
+        # Try/except is included because not all articles can be parsed
+        try:
+            # If article can be successfully parsed then insert its text, title, publish_date, keywords
+            # and summary into corresponding keys
+            art.parse()
+            article_dict["text"] = art.text
+            article_dict["title"] = art.title
+            article_dict["date"] = art.publish_date
+            art.nlp()
+            article_dict["keywords"] = art.keywords
+            article_dict["summary"] = art.summary
+        except newspaper.article.ArticleException:
+            # If article cannot be parse then insert null values for the following keys:
+            # "text", "title", "date", "keywords", and "summary"
+            article_dict["text"] = np.nan
+            article_dict["title"] = np.nan
+            article_dict["date"] = np.nan
+            article_dict["keywords"] = np.nan
+            article_dict["summary"] = np.nan
+
+        # Insert dictionary of article info into the articles_info list
+        articles_info.append(article_dict)
+    # Pass the list of dictionaries into a pandas data frame
+    corpus = pd.DataFrame(articles_info)
+    # Print how long the process took
+    print("Script took {:.2f} seconds to complete".format(time.time() - start))
+    return corpus
+
+
+def yahoonewsdata(option):
+    print('Getting news data for ',option)
+    url = "https://finance.yahoo.com/quote/%s/?p=%s" % (option, option)
+    links = get_news_headlines(url)
+    df = get_articles(links)
+    return df
+
+def getstocktwitsdata(option):
+    url = "https://api.stocktwits.com/api/2/streams/symbol/%s.json?limit=200"%(option)
+    #url = "https://api.stocktwits.com/api/2/streams/symbol/" + ticker + ".json"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    stocktwitsdata = pd.DataFrame(data['messages'])
+    return stocktwitsdata
+
+
+
---
+++ index.py
@@ -0,0 +1,186 @@
+from app import app
+from app import server
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+from datetime import datetime
+import dash_table
+import pandas as pd
+from data import yahoonewsdata,get_coin_data,getstocktwitsdata
+
+colors = {
+    'background': '#000000',
+    'background2': '#FF0',
+    'text': 'yellow'
+    }
+coin_list = ['AMZN','WMT','COST','KR']
+
+
+app.layout = html.Div([html.H1('Retail Innovation Center Stock Analysis Dashboard',
+                               style={
+                                      'textAlign': 'center',
+                                      "background": "yellow"}),
+               html.Div(['Select Dates',
+               dcc.DatePickerRange(
+                   id='date-input',
+                   stay_open_on_select=False,
+                   min_date_allowed=datetime(2013, 4, 28),
+                   max_date_allowed=datetime.now(),
+                   initial_visible_month=datetime.now(),
+                   start_date=datetime(2019, 1, 1),
+                   end_date=datetime.now(),
+                   number_of_months_shown=2,
+                   month_format='MMMM,YYYY',
+                   display_format='YYYY-MM-DD',
+                   style={
+                          'color': '#11ff3b',
+                          'font-size': '18px',
+                          'margin': 0,
+                          'padding': '8px',
+                          'background': 'yellow',
+                   }
+               ),
+               '-|- Select Ticker here',
+               dcc.Dropdown(id='dropdown',
+                            options=[{'label': i, 'value': i} for i in coin_list],
+                            value='AMZN',
+                            optionHeight=20,
+                            style={
+                                'height': '50px',
+                                'font-weight': 100,
+                                'font-size': '16px',
+                                'line-height': '10px',
+                                'color': 'gray',
+                                'margin': 0,
+                                'padding': '8px',
+                                'background': 'yellow',
+                                'position': 'middle',
+                                'display': 'inline-block',
+                                'width': '150px',
+                                'vertical-align': 'middle',
+                                }
+                            ),
+                html.Div(id='date-output'),
+                html.Div(id='intermediate-value', style={'display': 'none'}),
+                               ], className="row ",
+                    style={'marginTop': 0, 'marginBottom': 0, 'font-size': 30, 'color': 'white',
+                           'display': 'inline-block'}),
+
+                html.Div([
+                dcc.Tabs(id='tabs-example', value='tab-1', children=[
+                    dcc.Tab(label='Stock Price', value='tab-1'),
+                    dcc.Tab(label='Stock News -Ticker', value='tab-2'),
+                ]),
+                html.Div(id='tabs-example-content')
+                ]),
+
+
+               html.Div(children=[dcc.Markdown(" Retail Tech")], style={'textAlign': 'center',"background": "yellow"}),
+                              ],
+              style={"background": "#000080"}
+                            )
+
+@app.callback(Output('tabs-example-content', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.Div(id='graph-output', style={"height" : "50%", "width" : "50%"}),
+            html.Div(children=[html.H1(children="Stock Twits Data",
+                                       style={
+                                           'textAlign': 'left',
+                                           "background": "yellow"})
+                               ]
+                     ),
+            html.Div(children=[html.Table(id='stocktwits'),
+                               html.Div(id='stocktwits-output', style={"height": "100%", "width": "100%"})])
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.Div(children=[html.Table(id='table'), html.Div(id='table-output',style={"height" : "100%", "width" : "100%"})])
+        ])
+
+
+@app.callback(Output('table-output', 'children'),
+              [Input('dropdown', 'value')])
+def get_data_table(option):
+    df = yahoonewsdata(option)
+    df2 = df[['date','title','summary']]
+    df = pd.DataFrame(df2)
+    data_table = dash_table.DataTable(
+
+        id='datatable-data',
+        data=df.to_dict('records'),
+        columns=[{'id': c, 'name': c} for c in df.columns],
+        style_table={'overflowY': 'auto','maxHeight' : 700 },
+        style_header={'backgroundColor': 'rgb(0,250,154)'},
+        style_cell={'font_size': '26px', 'backgroundColor': 'rgb(32,178,170)','textAlign': 'left','whiteSpace': 'normal',
+            'height': 'auto'},
+    )
+    return data_table
+
+
+@app.callback(Output('stocktwits-output', 'children'),
+              [Input('dropdown', 'value')])
+def get_data_table(option):
+    df2 = getstocktwitsdata(option)
+    df = df2[['created_at','body']]
+    #df = pd.DataFrame(df2)
+    data_table = dash_table.DataTable(
+
+        id='datatable-data',
+        data=df.to_dict('records'),
+        columns=[{'id': c, 'name': c} for c in df.columns],
+        style_table={'overflowY': 'auto','maxHeight' : 700 },
+        style_header={'backgroundColor': 'rgb(0,250,154)'},
+        style_cell={'font_size': '26px', 'backgroundColor': 'rgb(32,178,170)','textAlign': 'left','whiteSpace': 'normal',
+            'height': 'auto'},
+    )
+    return data_table
+
+
+
+
+@app.callback(Output('graph-output', 'children'),
+              [Input('date-input', 'start_date'),
+               Input('date-input', 'end_date'),
+               Input('dropdown', 'value')])
+def render_graph(start_date, end_date, option):
+    df = get_coin_data(crypto=option, start_date='2013-04-28', end_date='2020-04-28', save_data=None)
+    data = df[(df.date >= start_date) & (df.date <= end_date)]
+    return dcc.Graph(
+        id='graph-1',
+        figure={
+            'data': [
+                {'x': data['date'], 'y': data['price'], 'type': 'line', 'name': 'value1'},
+            ],
+            'layout': {
+                'title': 'Price Vs Time ',
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text'],
+                    'size': 18
+                },
+                'xaxis': {
+                        'title': 'Time',
+                        'showspikes': True,
+                        'spikedash': 'dot',
+                        'spikemode': 'across',
+                        'spikesnap': 'cursor',
+                        },
+                'yaxis': {
+                        'title': 'Price',
+                        'showspikes': True,
+                        'spikedash': 'dot',
+                        'spikemode': 'across',
+                        'spikesnap': 'cursor'
+                        },
+
+            }
+        }
+    )
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
